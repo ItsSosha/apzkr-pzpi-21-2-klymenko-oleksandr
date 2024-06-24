0de66ff0d79486e7e12c2f3f862c5981e3cd8d9e
ItsSosha <whoissosha@gmail.com>
2024-06-24 23:41:15 +0200

    feat: add auth refresh interceptor

--- apzkr-pzpi-21-2-klymenko-oleksandr/Task3-WebClient/src/lib/axios.ts
+++ apzkr-pzpi-21-2-klymenko-oleksandr/Task3-WebClient/src/lib/axios.ts
@@ -19,6 +19,27 @@ api.interceptors.request.use(
   }
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response?.status === 401) {
+      const authState = auth.currentUser;
+      if (authState) {
+        try {
+          idToken = await authState.getIdToken(true);
+          error.config.headers.Authorization = `Bearer ${idToken}`;
+          return api.request(error.config);
+        } catch (refreshError) {
+          console.error("Token refresh failed:", refreshError);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 auth.onAuthStateChanged(async (authState) => {
   idToken = authState ? await await authState.getIdToken() : null;
 });
